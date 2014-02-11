be8a7fddca8cf2387344341b8e9400e3a1f944d8
Elicia <elicia_cor@hotmail.com>
2014-02-11 13:13:10 -0500

    Final demo app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,40 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  # GET /users
+  # GET /users.json
   def index
-    @users = User.all      
+    @users = User.all
   end
 
+  # GET /users/1
+  # GET /users/1.json
   def show
   end
 
+  # GET /users/new
   def new
     @user = User.new
   end
 
+  # GET /users/1/edit
   def edit
   end
 
+  # POST /users
+  # POST /users.json
   def create
     @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # PATCH/PUT /users/1
@@ -51,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:nombre, :email)
+      params.require(:user).permit(:name, :email)
     end
 end
